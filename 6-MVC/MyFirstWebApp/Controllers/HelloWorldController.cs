34ca2c7324628999a075c9f0511d3b75a1aaaa6c
yubinfeng <bifke@foxmail.com>
2016-02-28 01:08:19 +0800

    增加MVC6项目

---
+++ 6-MVC/MyFirstWebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyFirstWebApp.Controllers
+{
+    public class HelloWorldController : Controller
+    { 
+
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "这是我的默认动作...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "我是一个欢迎方法...";
+        }
+        public string Welcome2(string name, int ID = 1)
+        {           
+            return HtmlEncoder.Default.HtmlEncode(
+                "hello,name:" + name + ",id:" + ID);
+        }
+    }
+}
